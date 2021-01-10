1c859a65a85e591413bec68b5cd8c825a122249b
overaction <52850392+overaction@users.noreply.github.com>
2021-01-10 23:31:55 +0900

    5. create post and add middleware

--- server/graphql/resolvers/posts.js
+++ server/graphql/resolvers/posts.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Post = mongoose.model("Post");
 
+const checkAuth = require('../../util/check-auth');
+
 module.exports.postResolver = {
     Query: {
         async getPosts() {
@@ -10,6 +12,33 @@ module.exports.postResolver = {
             } catch(err) {
                 console.log(err);
             }
+        },
+        async getPost(_,{postId}) {
+            try {
+                const post = await Post.findById(postId);
+                if(post) {
+                    return post;
+                }
+                else {
+                    throw new Error('Post not found')
+                }
+            } catch(err) {
+                throw new Error(err);
+            }
+        }
+    },
+    Mutation: {
+        async createPost(_,{body}, context) {
+            checkAuth(context);
+            const userinfo = context.req.userinfo;
+            const newPost = new Post({
+                body,
+                user: userinfo.id,
+                username: userinfo.username,
+                createdAt: new Date().toISOString()
+            });
+            const post = await newPost.save();
+            return post;
         }
     }
 }
---
+++ server/util/check-auth.js
@@ -0,0 +1,17 @@
+const {AuthenticationError} = require('apollo-server');
+const jwt = require('jsonwebtoken');
+const {SECRET_KEY} = require('../config/keys');
+module.exports = (context,res,next) => {
+    const {authorization} = context.req.headers;
+    if(!authorization) {
+        throw new Error('Authentication header must be provided');
+    }
+    const token = authorization.replace("Bearer ", "");
+    jwt.verify(token, SECRET_KEY, (err,payload) => {
+        if(err) {
+            console.log(err);
+            throw new AuthenticationError('유효하지 않은 토큰',err);
+        }
+        context.req.userinfo = payload;
+    })
+}
